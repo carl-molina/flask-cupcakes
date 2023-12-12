c919784fda5bb47fe4fabec5b1d5f14e7c41d87b
Nathan Rydel <nathanrydel@gmail.com>
2023-12-11 18:08:59 -0700

    update functions to render cupcake on page

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,32 +1,40 @@
-d;// TODO: function jQuery objects
+// TODO: function jQuery objects
 
-// TODO: Create HTML for each cupcake
-// TODO: Get list of cupcakes from API
-// TODO: Show list on HTML via generator
-
-const BASE_URL = '/api/cupcakes/';
+const BASE_URL = '/api/cupcakes';
 
 const $cupcakeForm = $("#new_cupcake_form");
-const $cupcakeList = $("#cupcake_list_container");
+const $cupcakeList = $("#cupcake-list-container");
 
+/**
+ * Gets the current cupcakes from the database
+ * @returns An array of Cupcake Objects
+ */
 
 async function getCupcakes() {
   console.debug('getCupcakes ran!');
-  const resp = await fetch(`${BASE_URL}`, {
-    method: "GET",
-  });
+  const resp = await fetch("api/cupcakes");
 
   const cupcakeData = await resp.json();
+
   console.log('This is cupcakeData', cupcakeData);
+  console.log('This is cupcakeData.cupcakes in getCupcakes: ', cupcakeData.cupcakes);
 
   return cupcakeData.cupcakes;
 }
 
+
+/**
+ *
+ * @param {object} cupcake - A singular cupcake object
+ * @returns HTML markup for each cupcake list element
+ */
+
 function generateCupcakeHTML(cupcake) {
+  console.debug("generateCupcakeHTML ran!");
   return `
     <div class="list-group" id=${cupcake.id}>
       <a
-        href="${BASE_URL}${cupcake.id}"
+        href="${BASE_URL}/${cupcake.id}"
         class="list-group-item list-group-item-action list-group-item-primary"
       >
         ${cupcake.flavor} - Rating: ${cupcake.rating}
@@ -36,18 +44,34 @@ function generateCupcakeHTML(cupcake) {
 }
 
 
-
+/**
+ * Generate the HTML for each cupcake in database and append to $cupcakeList
+ * @param {obj[]} cupcakes - Array of Cupcake Objects
+ * DOES NOT RETURN, converts HTML in into jQuery object and appends
+ * to $cupcakeList
+ */
 
 function putCupcakesOnPage(cupcakes) {
+  console.debug("putCupcakesOnPage ran!");
+  // $cupcakeList.empty();
 
-  $cupcakeList.empty();
-
-  for (const cupcake of cupcakes) {
-    flavor = cupcake.flavor;
-    size = cupcake.size;
-    rating = cupcake.rating;
-    image_url = cupcake.image_url;
+  for (let cupcake of cupcakes) {
+    let $cupcake = generateCupcakeHTML(cupcake);
+    // flavor = cupcake.flavor;
+    // size = cupcake.size;
+    // rating = cupcake.rating;
+    // image_url = cupcake.image_url;
+    $cupcakeList.append($cupcake);
   }
-
-
 }
+
+/**
+ * Starter helper function
+ */
+
+async function start() {
+  const cupcakes = await getCupcakes();
+  putCupcakesOnPage(cupcakes);
+}
+
+start();
